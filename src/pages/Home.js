5ff231cd7576c7c14c6e513a3dbf642604e05a63
Rohit Kumar <rohit.kumar@o9solutions.com>
2021-02-14 13:55:48 +0530

    Added input filter to tables

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,26 @@
 import React, { useState, useEffect } from 'react'
 import './style.css';
+import TextField from '@material-ui/core/TextField';
 import { Link } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
+
 
 export default function Home() {
 
     const [users, setUsers] = useState([]);
+    const [fiteredUsers, setFilteredUsers] = useState([]);
+    const classes = useStyles();
+
+
 
     useEffect(()=>{
         fetchUsers();
@@ -14,12 +30,39 @@ export default function Home() {
         const data = await fetch('https://jsonplaceholder.typicode.com/users');
         const users = await data.json();
         setUsers(users);
+        setFilteredUsers(users);
         console.log(users);
+    }
+    const filterUsers = (e)=>{
+        let curUserText = e.target.value;
+        console.log(e.target.value);
+        let curFilteredUsers = users;
+        if(curUserText.length > 0){
+            const regex = new RegExp(`^${curUserText}`, 'i');
+            curFilteredUsers = users.sort().filter(user=>regex.test(user.name));
+        }
+        setFilteredUsers(()=>curFilteredUsers);
+    }
+
+    const filterCompanies = (e)=>{
+        let curCompanyText = e.target.value;
+        console.log(e.target.value);
+        let curFilteredUsers = users;
+        if(curCompanyText.length > 0){
+            const regex = new RegExp(`^${curCompanyText}`, 'i');
+            curFilteredUsers = users.sort().filter(user=>regex.test(user.company.name));
+        }
+        setFilteredUsers(()=>curFilteredUsers);
     }
 
     return (
         <div>
             <h1>Home Page</h1>
+            <form className={classes.root} noValidate autoComplete="off">
+                <TextField id="standard-basic" label="User Name" onChange={filterUsers} />
+                <TextField id="standard-basic" label="Company Name" onChange={filterCompanies} />
+            </form>
+
             <table>
                 <thead>
                     <tr>
@@ -29,7 +72,7 @@ export default function Home() {
                     </tr>
                 </thead>
                 <tbody>
-                    {users.map(user=>(
+                    {fiteredUsers.map(user=>(
                         <tr key={user.id}>
                             <td>{user.name}</td>
                             <td>{user.company.name}</td>
--- src/pages/Posts.js
+++ src/pages/Posts.js
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom';
 import './style.css'
+import TextField from '@material-ui/core/TextField';
 
 export default function Posts({ match }) {
     
     const [posts, setPosts] = useState([]);
+    const [filteredPosts, setFilteredPosts] = useState([]);
+    
 
     useEffect(() => {
         fetchPosts();
@@ -18,6 +21,17 @@ export default function Posts({ match }) {
         //console.log(Object.prototype.toString.call(posts));
         console.log(userPosts);
         setPosts(userPosts);
+        setFilteredPosts(userPosts);
+    }
+
+    const filterPosts = (e)=>{
+        let curPostText = e.target.value;
+        let curFilteredPosts = posts;
+        if(curPostText.length > 0){
+            const regex = new RegExp(`^${curPostText}`, 'i');
+            curFilteredPosts = posts.sort().filter(post=>regex.test(post.title));
+        }
+        setFilteredPosts(()=>curFilteredPosts);
     }
 
     return (
@@ -32,19 +46,24 @@ export default function Posts({ match }) {
                 <a href="#">&raquo;</a>
             </div>
             <br /><br />
-            <p><b>*Use Pagination to go to post details</b></p>
+            <p><b>*Use Pagination or link to go to post details</b></p>
+
+            <TextField id="standard-basic" label="Post Title" onChange={filterPosts} />
+
             <table>
                 <thead>
                     <tr>
                         <th>Post No.</th>
                         <th>Post Tiltle</th>
+                        <th>Post Details</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {posts.map(post=>(
+                    {filteredPosts.map(post=>(
                         <tr key={post.id}>
                             <td>{post.id}</td>
                             <td>{post.title}</td>
+                            <td><Link id="links" to={`/posts/${post.id}`}>Go to post {post.id}</Link></td>
                         </tr>
                     ))}
                 </tbody>
